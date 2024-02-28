84bdb0e4cffb5d02b692ebfe6e81c02120098285
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-02-29 00:22:10 +0900

    [Config] .gitignore 추가

---
+++ Unsplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplash
+//
+//  Created by 김수경 on 2024/02/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
