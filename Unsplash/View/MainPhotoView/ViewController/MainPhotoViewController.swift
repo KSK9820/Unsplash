673356e4345b85263a7fae39c8e6655f3a80b9dd
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-03-26 16:58:44 +0900

    [Refactor] UICollectionView에 대한 delegate와 datasource를 UIViewController가 갖고 있게 변경

--- Unsplash/View/MainPhotoView/ViewController/MainPhotoViewController.swift
+++ Unsplash/View/MainPhotoView/ViewController/MainPhotoViewController.swift
@@ -11,27 +11,35 @@ final class MainPhotoViewController: UIViewController {
     
     private let viewModel = MainPhotoViewModel()
     
-    private lazy var recentCollectionView = RecentImageCollectionView(viewModel, presenterDelegate: self)
+    private lazy var recentCollectionView = RecentImageCollectionView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        UISetting()
-        UIConfigure()
-
-        currentPageBinding()
-        totalPageBinding()
+        setUI()
+        configureUI()
+        
+        viewModel.getPhotoInformation()
+        bindData()
     }
     
     
     // MARK: - private method
     
-    private func UISetting() {
+    private func setUI() {
         view.backgroundColor = .white
+        
+        let layout = RecentCollectionViewLayout()
+        layout.delegate = self
+        recentCollectionView.collectionViewLayout = layout
+        
+        recentCollectionView.delegate = self
+        recentCollectionView.dataSource = self
+        recentCollectionView.prefetchDataSource = self
     }
     
-    private func UIConfigure() {
+    private func configureUI() {
         let safeArea = view.safeAreaLayoutGuide
 
         view.addSubview(recentCollectionView)        
@@ -44,22 +52,90 @@ final class MainPhotoViewController: UIViewController {
         ])
     }
     
-    private func totalPageBinding() {
-        viewModel.totalPage.bind { _ in
+    private func bindData() {
+        viewModel.photoInformation.bind { _ in
             DispatchQueue.main.async {
                 self.recentCollectionView.reloadData()
             }
         }
     }
-    
-    private func currentPageBinding() {
-        viewModel.currentPage.bind { [weak self] _ in
-            if self?.viewModel.currentPage.value == self?.viewModel.totalPage.value {
-                self?.viewModel.getPhotoInformation()
-            }
-        }
-    }
-    
+
 }
 
 
+// MARK: - UICollectionViewDelegate method
+
+extension MainPhotoViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailPhotoViewControoler = DetailPhotoViewController(id: viewModel.getPhotoID(index: indexPath.row))
+    
+        self.navigationController?.pushViewController(detailPhotoViewControoler, animated: true)
+    }
+}
+
+
+// MARK: - UICollectionViewDataSource method
+
+extension MainPhotoViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.getPhotoCount()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecentImageCollectionViewCell.reuseIdentifier, for: indexPath) as? RecentImageCollectionViewCell else {
+            return RecentImageCollectionViewCell()
+        }
+        
+        cell.backgroundColor = UIColor.systemGray
+        cell.setImage(urlString: viewModel.getThumbURLString(index: indexPath.row))
+        
+        if let description = viewModel.photoInformation.value[indexPath.row].altDescription {
+            cell.setTitle(string: description)
+        } else if let description = viewModel.photoInformation.value[indexPath.row].description {
+            cell.setTitle(string: description)
+        }
+        
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: MainPhotoHeaderView.reuseIdentifier, for: indexPath) as? MainPhotoHeaderView else {
+                return MainPhotoHeaderView()
+            }
+            
+            headerView.setTitle(with: "최신 이미지")
+           
+            return headerView
+        default:
+            assert(false)
+        }
+    }
+}
+
+
+// MARK: - UICollectionViewDataSourcePrefetching method
+
+extension MainPhotoViewController: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        for indexPath in indexPaths {
+            if viewModel.getPhotoCount() - 5 < indexPath.item && viewModel.fetchNextpage {
+                viewModel.getPhotoInformation()
+            }
+        }
+    }
+}
+
+
+// MARK: - Recent Photo Layout Delegate method
+
+extension MainPhotoViewController: RecentCollectionViewLayoutDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
+        return viewModel.getImageSize(row: indexPath.row, viewWidth: view.bounds.width)
+    }
+    
+}
--- Unsplash/View/MainPhotoView/ViewController/View/RecentImageCollectionView.swift
+++ Unsplash/View/MainPhotoView/ViewController/View/RecentImageCollectionView.swift
@@ -8,18 +8,12 @@
 import UIKit
 
 final class RecentImageCollectionView: UICollectionView {
-
-    private let viewModel: MainPhotoViewModel
     
-    weak var presenterDelegate: UIViewController?
-    
-    init(_ viewModel: MainPhotoViewModel, presenterDelegate: UIViewController) {
-        self.viewModel = viewModel
-        self.presenterDelegate = presenterDelegate
-        
+    init() {
         super.init(frame: .zero, collectionViewLayout: UICollectionViewLayout())
-        configuration()
+        configureUI()
     }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -28,98 +22,10 @@ final class RecentImageCollectionView: UICollectionView {
     
     // MARK: - private method
     
-    private func configuration() {
-        let layout = RecentCollectionViewLayout()
-        layout.delegate = self
-        
-        self.collectionViewLayout = layout
-        
+    private func configureUI() {
         self.register(MainPhotoHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: MainPhotoHeaderView.reuseIdentifier)
         self.register(RecentImageCollectionViewCell.self, forCellWithReuseIdentifier: RecentImageCollectionViewCell.reuseIdentifier)
-        
-        self.dataSource = self
-        self.delegate = self
-        self.prefetchDataSource = self
         self.translatesAutoresizingMaskIntoConstraints = false
     }
 
-}
-
-
-// MARK: - UICollectionViewDataSource method
-
-extension RecentImageCollectionView: UICollectionViewDataSource {
-    
-    // pagination 오류
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        viewModel.getPhotoCount()
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecentImageCollectionViewCell.reuseIdentifier, for: indexPath) as? RecentImageCollectionViewCell else {
-            return RecentImageCollectionViewCell()
-        }
-        
-        cell.backgroundColor = UIColor.systemGray
-        cell.setImage(urlString: viewModel.getThumbURLString(index: indexPath.row))
-        cell.setTitle(string: viewModel.getTitleString(index: indexPath.row))
-        
-        return cell
-    }
-
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        switch kind {
-        case UICollectionView.elementKindSectionHeader:
-            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: MainPhotoHeaderView.reuseIdentifier, for: indexPath) as? MainPhotoHeaderView else {
-                return MainPhotoHeaderView()
-            }
-            
-            headerView.setTitle(with: "최신 이미지")
-           
-            return headerView
-        default:
-            assert(false)
-        }
-    }
-    
-}
-
-
-// MARK: - UICollectionViewDelegate method
-
-extension RecentImageCollectionView: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let detailPhotoViewControoler = DetailPhotoViewController(id: viewModel.getPhotoID(index: indexPath.row))
-        
-        detailPhotoViewControoler.modalPresentationStyle = .overCurrentContext
-        
-        presenterDelegate?.present(detailPhotoViewControoler, animated: true)
-    }
-}
-
-
-// MARK: - UICollectionViewDataSourcePrefetching method
-
-extension RecentImageCollectionView: UICollectionViewDataSourcePrefetching {
-
-    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
-        for indexPath in indexPaths {
-            if viewModel.getPhotoCount() - 5 < indexPath.item && viewModel.currentPage.value < viewModel.totalPage.value {
-                viewModel.fetchNextPage()
-            }
-        }
-    }
-    
-}
-
-
-// MARK: - Recent Photo Layout Delegate method
-
-extension RecentImageCollectionView: RecentCollectionViewLayoutDelegate {
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
-        return viewModel.getImageSize(row: indexPath.row, viewWidth: self.layer.bounds.width)
-    }
-    
 }
